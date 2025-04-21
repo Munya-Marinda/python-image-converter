0db62acbef3e58e4078e88c96ebd94015bd20465
Munya-Marinda <moyomarinda@gmail.com>
2025-04-22 00:43:38 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+
+input_folder = 'images'
+output_folder = 'converted_pngs'
+os.makedirs(output_folder, exist_ok=True)
+
+for file_name in os.listdir(input_folder):
+    if file_name.endswith('.webp'):
+        webp_path = os.path.join(input_folder, file_name)
+        png_path = os.path.join(output_folder, file_name.replace('.webp', 
+'.png'))
+
+        with Image.open(webp_path) as img:
+            img.save(png_path, 'PNG')
+            print(f"Converted: {file_name}")
+
